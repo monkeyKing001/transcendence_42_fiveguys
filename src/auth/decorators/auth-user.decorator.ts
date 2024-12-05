52376d3597a67cc8ea2730a0c1e962f3c4999246
monkeyking <2018121152@yonsei.ac.kr>
2024-12-05 19:24:35 +0900

    rebase: Add full(back, front) src

--- src/auth/decorators/auth-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const currentAuthUser = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-	request.session.id;
-    return data ? request.user?.[data] : request.user;
-  },
-);
--- src/auth/guards/auth.fortytwoGuard.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-//how can pass the guard?
-//it will pass if the strategy validate function pass the request!!
-@Injectable()
-export class FortyTwoAuthGuard extends AuthGuard("42") {}
